2308df8d6f8ca228f9766a9ec4a1dfe110137964
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-05-04 22:04:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//let increment=0;
+
+function increment(){
+    
+    let increment=0;
+    increment+=1
+
+
+}
+
+increment()
+
+console.log(increment);
---
+++ index1.js
@@ -0,0 +1,28 @@
+var num1 =5;
+var num2 =10;
+
+var num1=document.getElementById("num1").value;
+
+var num2=document.getElementById("num2").textContent;
+var num3 =10;
+
+function sum() {
+num3=num1+num2;
+console.log(num3);
+
+    
+}
+
+let num1=10; 
+let num2=10;
+
+let num1=document.getElementById("num1").textContent;
+let num2=document.getElementById("num2").textContent;
+
+let num3=10;
+
+function sum() 
+{num3=num2+num1;
+    console.log(num3);
+    
+}
